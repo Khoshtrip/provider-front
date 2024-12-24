bd847f5afd3272ede6e332959e886b8e5716f051
Amir Ravan <amir.h.rnn@gmail.com>
2024-12-24 19:04:25 +0330

    Update Main Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,58 +6,71 @@ import Login from "./Login";
 import { useNavigate } from "react-router-dom";
 import "../styles/Header.css";
 
-const Header = () => {
-    const { isAuthenticated, user, logout } = useContext(AuthContext);
-    const [showLoginSignup, setShowLoginSignup] = React.useState(false);
+const AuthenticationButtons = ({ isAuthenticated, setShowLoginSignup }) => {
     const navigate = useNavigate();
+    const { logout } = useContext(AuthContext);
+    return isAuthenticated ? (
+        <>
+            <Button
+                variant="outline-primary"
+                className="me-2 on-primary border-on-primary"
+                onClick={() => {
+                    navigate("/profile");
+                }}
+            >
+                Profile
+            </Button>
+            <Button
+                variant="primary"
+                className="on-primary border-on-primary"
+                onClick={() => {
+                    navigate("/");
+                    logout();
+                }}
+            >
+                Logout
+            </Button>
+        </>
+    ) : (
+        <Button
+            variant="outline-primary"
+            onClick={() => setShowLoginSignup(true)}
+            className="on-primary border-on-primary"
+        >
+            Login
+        </Button>
+    );
+};
+
+const Header = () => {
+    const { isAuthenticated, logout } = useContext(AuthContext);
+    const [showLoginSignup, setShowLoginSignup] = React.useState(false);
 
     return (
         <>
-            <Navbar expand="sm">
+            <Navbar expand="sm" className="bg-primary on-primary">
                 <Container fluid>
-                    <Navbar.Brand as={Link} to="/">
+                    <Navbar.Brand as={Link} to="/" className="on-primary">
                         KhoshTrip
                     </Navbar.Brand>
                     <Navbar.Toggle aria-controls="navbar-nav" />
                     <Navbar.Collapse id="navbar-nav">
-                        <Nav className="ms-auto">
-                            {isAuthenticated ? (
-                                <>
-                                    <Button
-                                        variant="primary"
-                                        className="me-2"
-                                        onClick={() => {
-                                            navigate("/profile");
-                                        }}
-                                    >
-                                        Profile
-                                    </Button>
-                                    <Button
-                                        variant="primary"
-                                        onClick={() => {
-                                            navigate("/");
-                                            logout();
-                                        }}
-                                    >
-                                        Logout
-                                    </Button>
-                                </>
-                            ) : (
-                                <Button
-                                    variant="primary"
-                                    onClick={() => setShowLoginSignup(true)}
-                                >
-                                    Login
-                                </Button>
-                            )}
+                        <Nav className="me-auto my-2 my-lg-0">
+                            <Nav.Link href="/" className="on-primary">
+                                Home
+                            </Nav.Link>
+                            <Nav.Link href="/products" className="on-primary">
+                                Products
+                            </Nav.Link>
                         </Nav>
+                        <AuthenticationButtons
+                            isAuthenticated={isAuthenticated}
+                            setShowLoginSignup={setShowLoginSignup}
+                        />
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
 
-            {/* {showLoginSignup && (
-                <Login onClose={() => setShowLoginSignup(false)} />
-            )} */}
             <Login
                 show={showLoginSignup}
                 onHide={() => {
---
+++ src/pages/Products.js
@@ -0,0 +1,13 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext";
+import "../styles/Products.css";
+
+const Products = () => {
+    const { user } = useContext(AuthContext);
+
+    return (
+
+    );
+};
+
+export default Products;
